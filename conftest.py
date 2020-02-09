986efeae5ab08b9e7f43247c245cf6e8e4e0a374
novdima-github <bbs.minsk@gmail.com>
2020-02-09 16:49:07 +0300

    Run tests with multilanguages parameter

--- conftest.py
+++ conftest.py
@@ -1,31 +1,43 @@
+"""Run test:
+(selenium_env) F:\***\Python_tutorial\Stepic-multilanguages>pytest
+--language=es --browser_name=firefox test_items.py
+Browser name is not obligatory parameter
+"""
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    """Опции командной строки. В командную строку передается параметр вида
-    '--language="es"' По умолчанию передается параметр, включающий английский
-    интерфейс в браузере
-    """
-    parser.addoption('--language', action='store', default="en",
-                     help="Choose language")
+    parser.addoption('--browser_name',
+                     action='store',
+                     default='chrome',
+                     help="with this option choose browser you use to make tests \
+                          'chrome' for Google Chrome, 'firefox' for FireFox")
+    parser.addoption('--language',
+                     action='store',
+                     default=None,
+                     help="choose local language with this option:\
+                          you may choose among 'ru','en', etc..")
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='function')
 def browser(request):
-    # В переменную user_language передается параметр из командной строки
-    user_language = request.config.getoption("language")
-
-    # Инициализируются опции браузера
-    options = Options()
-
-    # В опции вебдрайвера передаем параметр из командной строки
-    options.add_experimental_option('prefs',
-                                    {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-
-    browser.implicitly_wait(5)
+    browser_name = request.config.getoption('browser_name')
+    usr_language = request.config.getoption('language')
+    browser = None
+    if browser_name =='chrome':
+        print('\nstarting Chrome browser for testing..')
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': usr_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == 'firefox':
+        print('\nstarting FireFox browser for testing..')
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_languages', usr_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError('"--browser_name" parameter has to be "chrome" or "ffox"')
     yield browser
-    print("\nquit browser..")
+    print('\nquit browser..')
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,6 +1,16 @@
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_check_add_to_cart(browser):
+def test_add_to_cart_button_exist(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+    time.sleep(5)
+    add_to_cart_button = WebDriverWait(browser, 5).\
+        until(EC.presence_of_element_located((By.CSS_SELECTOR, '.btn-add-to-basket')))
+    assert add_to_cart_button, "No 'Add to basket button exist'..."
